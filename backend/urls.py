e9f6bb89b2cf30077e61752d12f90631b269e79c
mobilesuitlovelive <gundamjack@126.com>
2018-06-23 21:15:33 +0800

    update stage api

--- backend/urls.py
+++ backend/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('movie/all', backend_view.movie_showall),
     path('cinema/search', backend_view.cinema_search),
     path('cinema/available_movies', backend_view.available_movies_in_cinema),
+    path('cinema/get_stage_info', backend_view.get_stage_info),
     path('cinema/available_food_suppliers', backend_view.available_food_suppliers_in_cinema),
     path('food_suppliers/available_services', backend_view.available_food_services_by_supplier),
     path('ticket/post',backend_view.tiket_post),
